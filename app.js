b6eedc65129da46bda16e7050f0343cb969b9b6d
Illia-Sokol <illiasokolovskyi@gmail.com>
2021-02-04 22:32:31 +0100

    start node app

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var dealsRouter = require('./routes/deals');
 
 var app = express();
+// const WebSocket = requier('ws');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/deals', dealsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +38,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// create ws
+// const connection = new WebSocket.server()
 
 module.exports = app;
